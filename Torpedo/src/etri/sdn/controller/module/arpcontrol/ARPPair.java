c5dd69d845f77e473445bc2719030662ed2a010d
hyunjeong <hoonai14@kaist.ac.kr>
2014-05-29 09:56:00 +0900

    arppair added

---
+++ Torpedo/src/etri/sdn/controller/module/arpcontrol/ARPPair.java
@@ -0,0 +1,11 @@
+package etri.sdn.controller.module.arpcontrol;
+
+
+public class ARPPair {
+	String ip;
+	String mac;
+	public ARPPair(String ip, String mac){
+		this.ip = ip;
+		this.mac = mac;
+	}
+}
